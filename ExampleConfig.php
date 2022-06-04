ddeedeb5d823f0f3de09a2dc5250aa38d814c87c
TimG6 <106842968+TimG6@users.noreply.github.com>
2022-06-05 00:30:54 +0200

    Add files via upload

---
+++ ExampleConfig.php
@@ -0,0 +1,7 @@
+<?php
+    //change all the variables to your needs and rename this file to Config.php if you are going to use it
+    $database_host=""; //location of your database
+    $database_name="dosegrpt"; 
+    $database_username=""; //type in your username
+    $database_password=""; //type in your password
+?>
